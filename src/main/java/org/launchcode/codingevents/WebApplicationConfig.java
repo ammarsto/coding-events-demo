c7acbb6cfef9c0aa454fd0dc5e4731fa183f6701
Amy Stockhausen <stockhausenamy@gmail.com>
2023-03-23 20:38:49 -0500

    Parts 1-3 of Studio

---
+++ src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
@@ -0,0 +1,20 @@
+package org.launchcode.codingevents;
+
+import org.launchcode.codingevents.controllers.AuthenticationController;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(AuthenticationFilter);
+    }
+
+}
